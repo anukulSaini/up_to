d489c4cc8f773e57408e14773e9514b05fdcd020
anukulSaini <anukulsaini2295@gmail.com>
2021-02-12 19:38:29 +0530

    imported header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+//By Anukul Saini
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css"
 
-
 ReactDOM.render(
- 
-    <App />
-  ,
+    <App /> ,
   document.getElementById('root')
 );
 
