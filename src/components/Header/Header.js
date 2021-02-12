102ef49555b5f2cca4c419f575122334f621fe86
anukulSaini <anukulsaini2295@gmail.com>
2021-02-12 13:55:42 +0530

    added component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../../hoc/Aux/Aux';
+
+const Header = () =>{
+    return (
+         <Aux>
+             <div className="header">j</div>
+         </Aux>
+    );
+}
+   
+export default Header;
