d016f83a270213bcd59933cf868ac542b2b9e2c4
anukulSaini <anukulsaini2295@gmail.com>
2021-02-12 19:36:35 +0530

    added logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
+import Logo from '../Logo/Logo';
+import './Header.css'
 
 const Header = () =>{
     return (
          <Aux>
-             <div className="header">j</div>
+             <div className="header">
+                <div className="Logo">
+                <Logo/>
+                </div>
+             </div>
          </Aux>
     );
 }
