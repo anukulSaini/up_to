7158d3c44809edb2c439f715afa8407be851c061
anukulSaini <anukulsaini2295@gmail.com>
2021-02-13 13:02:37 +0530

    import CreateNode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Aux from './hoc/Aux/Aux';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'; 
+import CreateNote from './components/CreateNote/CreateNote';
 
 function App() {
   return (
     <Aux>
          <Header></Header>
+         <Footer></Footer>
+         <CreateNote></CreateNote>
     </Aux>
   );
 }
