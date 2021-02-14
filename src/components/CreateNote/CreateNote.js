fdd4387dd16f392b8f887a4d4efbfb244fb0903f
anukulSaini <anukulsaini2295@gmail.com>
2021-02-14 14:13:38 +0530

    using useState for input

--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
-const CreateNote = () => {
+const CreateNote = (props) => {
+const [note,setNote] = useState({
+     title : "",
+     content : ""
+    })
+
+    const InputEvent = (event) =>{
+        // const value = event.target.value;
+        // const name = event.target.name;
+//Object Destructuring....
+
+         const {name,value} = event.target;
+
+         setNote((prevData)=>{
+            return {
+                ...prevData,
+                 [name]:value,
+            }
+         });
+           console.log(note);
+    }
+
+    const anukulhandle = ()=> {
+        props.passNote(note);
+    }
+
+    
     return (
         <div className="main_note">
                <form>
-                   <input type="text" placeholder="Title"></input>
-                   <textarea rows="" column="" placeholder="Write a note"></textarea>
+                 <input 
+                   type="text" 
+                   name="title"
+                   value={note.title}  
+                   onChange={InputEvent} 
+                   placeholder="Title" 
+                   autoComplete="off">
+                 </input>
+                   <textarea 
+                    rows=""
+                    column=""
+                    name="content"
+                    value={note.content}
+                    onChange={InputEvent}
+                    placeholder="Write a note">
+                   </textarea>
                </form>
-               <Button>
+           
+               <Button onClick={anukulhandle}>
                    <AddIcon className="plus_sign"></AddIcon>
                </Button>
         </div>
