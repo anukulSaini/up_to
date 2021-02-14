15d58b37dab6f7b5c5923f99129d64062a6bd88b
anukulSaini <anukulsaini2295@gmail.com>
2021-02-14 15:15:02 +0530

    Component Store data from CreateNote

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
  
-const Note = () => {
+const Note = (props) => {
     return ( 
         <Aux>
             <div className="note">
-               <h1>Title</h1>
+               <h1>{props.value}</h1>
                <p>
-                   This is the content
+                   {props.content}
                </p>
                <button className="btn">
                <DeleteOutlineIcon className="deleteIcon"/>
