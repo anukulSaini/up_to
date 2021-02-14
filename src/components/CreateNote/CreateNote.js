2f3ed75eccca3795ca29267b832fa02f5dcf465f
anukulSaini <anukulsaini2295@gmail.com>
2021-02-14 17:30:08 +0530

    added click and double click  functionality

--- src/components/CreateNote/CreateNote.js
+++ src/components/CreateNote/CreateNote.js
@@ -3,6 +3,9 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
 const CreateNote = (props) => {
+
+const [expand,setExpand] =useState(false);
+
 const [note,setNote] = useState({
      title : "",
      content : ""
@@ -34,8 +37,9 @@ const [note,setNote] = useState({
 
     
     return (
-        <div className="main_note">
+        <div className="main_note"  onDoubleClick={()=>setExpand(false)}>
                <form>
+                   {expand?
                  <input 
                    type="text" 
                    name="title"
@@ -44,19 +48,22 @@ const [note,setNote] = useState({
                    placeholder="Title" 
                    autoComplete="off">
                  </input>
+                 :null}
                    <textarea 
                     rows=""
                     column=""
                     name="content"
                     value={note.content}
                     onChange={InputEvent}
-                    placeholder="Write a note">
+                    placeholder="Write a note"
+                    onClick={()=>setExpand(true)}>
                    </textarea>
                </form>
-           
+           {expand?
                <Button onClick={anukulhandle}>
                    <AddIcon className="plus_sign"></AddIcon>
                </Button>
+               :null}
         </div>
     );
 }
