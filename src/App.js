69bca4b70e117d723fae35c872a645512b8ca9ca
anukulSaini <anukulsaini2295@gmail.com>
2021-02-14 14:12:27 +0530

    using useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //By Anukul Saini
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from './hoc/Aux/Aux';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer'; 
@@ -7,10 +7,20 @@ import CreateNote from './components/CreateNote/CreateNote';
 import Note from './components/Note/Note';
 
 function App() {
+
+  const [addItem,setAddItem] = useState([]);
+  
+  const addNote = (note) =>{
+    alert ("ok");
+    setAddItem((prevData) =>{
+      return [...prevData,note];
+    });
+  };
+
   return (
     <Aux>
          <Header></Header>
-         <CreateNote></CreateNote>
+         <CreateNote passNote ={addNote}></CreateNote>
          <Note></Note>
          <Footer></Footer>
     </Aux>
